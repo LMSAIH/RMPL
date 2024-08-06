8b577654e97a436d6c4afc2c074597913d19d865
Axel Velasquez <yoloax11@gmail.com>
2024-08-05 18:47:16 -0700

    Added the encoding and DB management (I forgot)

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express');
 require('dotenv').config();
+const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
+
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
-const expressLayout = require('express-ejs-layouts');
+
+connectDB();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 app.use('/', require('./server/routes/main'));
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`)
 });
 
-app.get('/', (req,res) => {
-    res.send("<h1>Hello</h1>");
-    console.log("request received");
-    console.log(process.env.NAME);
-});
+
 
 
