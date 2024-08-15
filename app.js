f95bd5fa4d50b1dd48697fda987a8b1dbd7098ac
Axel Velasquez <yoloax11@gmail.com>
2024-08-15 14:18:31 -0700

    updated app, working on user authentication

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 require('dotenv').config();
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./server/config/db');
+const session = require('express-session');
 
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT =  process.env.PORT || 5000;
 
 connectDB();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'Super secret',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+    }),
+}));
 
 app.use(expressLayout);
 app.set('layout', './layouts/main');
