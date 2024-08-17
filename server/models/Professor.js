c06fae1d64e13944b81f7c09024eb692e19aee43
Axel Velasquez <yoloax11@gmail.com>
2024-08-16 20:46:15 -0700

    updated the professor model to add comments

--- server/models/Professor.js
+++ server/models/Professor.js
@@ -1,44 +1,63 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const PostSchema = new Schema({
-    name:{
-        type:String,
-        required:true
+  name: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  ratings: {
+    type: Number,
+    required: true,
+  },
+  overall: {
+    type: Number,
+    required: true,
+  },
+  quality: {
+    type: Number,
+    required: true,
+  },
+  difficulty: {
+    type: Number,
+    required: true,
+  },
+  workload: {
+    type: Number,
+    required: true,
+  },
+  subjects: {
+    type: Array,
+    required: true,
+  },
+  comments: [
+    {
+      author: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
     },
-    department:{
-        type:String,
-        required:true
-    },
-    ratings:{
-        type:Number,
-        required:true
-    },
-    overall:{
-        type:Number,
-        required:true
-    },
-    difficulty:{
-        type:Number,
-        required:true
-    },
-    workload:{
-        type:Number,
-        required:true
-    },
-    subjects:{
-        type:Array,
-        required:true,
-    },
-    createdAt:{
-        type:Date,
-        default: Date.now
-    },
-    updatedAt:{
-        type:Date,
-        default:Date.now
-    }
-    
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Professor', PostSchema);
+module.exports = mongoose.model("Professor", PostSchema);
